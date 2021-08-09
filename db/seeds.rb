b65dc969680d025aed2773787b64f2c45b67dccc
tungchinlee <tungchinli@gmail.com>
2021-08-09 11:13:03 +0800

    seed logic edited

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-if User.none?
+if User.admin.none?
   user = User.create(
     name: "first_user", 
     email: "first@gmail.com", 
